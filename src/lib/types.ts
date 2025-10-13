ee3ff4086430e33e5a0171ee97a56221b188f9f5
wobsoriano <sorianorobertc@gmail.com>
2025-10-12 18:44:14 -0700

    chore: initial full blown tanstack lib

---
+++ src/lib/types.ts
@@ -0,0 +1,35 @@
+import type { DefaultError, QueryClient, QueryKey, QueryObserverOptions, QueryObserverResult } from "@tanstack/query-core"
+import { PropsWithChildren } from "ripple"
+
+export type QueryClientProviderProps = PropsWithChildren<{
+  client: QueryClient
+}>
+
+/** Options for createBaseQuery */
+export type CreateBaseQueryOptions<
+  TQueryFnData = unknown,
+  TError = DefaultError,
+  TData = TQueryFnData,
+  TQueryData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey,
+> = QueryObserverOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey>
+
+/** Result from createBaseQuery */
+export type CreateBaseQueryResult<
+  TData = unknown,
+  TError = DefaultError,
+> = QueryObserverResult<TData, TError>
+
+/** Options for createQuery */
+export type CreateQueryOptions<
+  TQueryFnData = unknown,
+  TError = DefaultError,
+  TData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey,
+> = CreateBaseQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>
+
+/** Result from createQuery */
+export type CreateQueryResult<
+  TData = unknown,
+  TError = DefaultError,
+> = CreateBaseQueryResult<TData, TError>
